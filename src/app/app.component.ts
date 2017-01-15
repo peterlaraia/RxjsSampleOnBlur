f4790984350809cbb1eb3e56441858ed4a911248
peterlaraia <inetrica@gmail.com>
2017-01-14 22:47:42 -0600

    PL - solution for this reddit user's question: learned about Observable.fromEvent() & .sample(): https://www.reddit.com/r/Angular2/comments/5o0175/help_needed_with_reactive_forms/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, NgModule, ElementRef, ViewChild} from '@angular/core'
+import {FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser'
+
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  @ViewChild('searchInput') inputEl: ElementRef;
+  valueOnBlur: string;
+  searchForm: FormGroup;
+  constructor() {
+    this.valueOnBlur = 'JIMMY';
+    this.searchForm = new FormGroup({
+      'name': new FormControl('Alice')
+    });
+  }
+  
+  ngAfterViewInit(): void {
+    console.log(this.inputEl);
+    this.searchForm.get('name').valueChanges
+    .sample(Observable.fromEvent(this.inputEl.nativeElement, 'blur'))
+    .subscribe((change: any) => this.valueOnBlur = change);
+    
+    //Observable.fromEvent(this.inputEl.nativeElement, 'blur').subscribe(() => console.log('BLURRED'));
+    //Observable.of('Boogers').subscribe((val: any) => console.log(val));
+    
+  }
 }
